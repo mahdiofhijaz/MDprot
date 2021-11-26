c943f8441c6cb231642b029812bcac60d88c7dd0
hijazi <mahdi.hijazi@epfl.ch>
2021-11-26 15:31:21 +0100

    Add Useful_tools, GPCR_tools, and entropy/MI calculation scripts to Entropy_MI

---
+++ Useful_tools/print2pdf.m
@@ -0,0 +1,14 @@
+function [file_name] = print2pdf(file_name)
+%printpdf Prints your chosen figure to pdf file
+%  * file_name is the name of the file
+
+%% print to pdf
+
+set(gcf,'Units','inches');
+screenposition = get(gcf,'Position');
+set(gcf,...
+    'PaperPosition',[0 0 screenposition(3:4)],...
+    'PaperSize',[screenposition(3:4)]);
+print(file_name,'-dpdf') 
+end
+
