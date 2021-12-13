23262e3b28e85c81052326eab60fa4499333121a
hijazi <mahdi.hijazi@epfl.ch>
2021-12-13 13:33:33 +0100

    print2pdb now can take an option to use openGL or not, which makes it more versatile for large figures

--- Useful_tools/print2pdf.m
+++ Useful_tools/print2pdf.m
@@ -1,15 +1,34 @@
-function [file_name] = print2pdf(file_name)
+function [file_name] = print2pdf(file_name,isOpengl)
 %printpdf Prints your chosen figure to pdf file
-%  * file_name is the name of the file
+%
+% Usage:
+% [file_name] = print2pdf(file_name)
+% [file_name] = print2pdf(file_name,isOpengl)
+%
+% * file_name is the name of the file
+% * isOpengl uses opengl renderer, this is not recommended unless you have
+% problems rendering otherwise, as it turns the pdf into an image.
 
 %% print to pdf
+
+if nargin < 2 % use normal rendering unless otherwise specified
+    isOpengl = 0;
+end
 
 set(gcf,'Units','inches');
 screenposition = get(gcf,'Position');
 set(gcf,...
     'PaperPosition',[0 0 screenposition(3:4)],...
     'PaperSize',[screenposition(3:4)]);
-% print(file_name,'-dpdf') 
- print(file_name, '-dpdf', '-opengl', '-r0');
+
+% set(gcf,'PaperPositionMode','Auto','PaperUnits','Inches','PaperSize',[screenposition(3), screenposition(4)])
+
+if isOpengl == 1
+    print(file_name, '-dpdf', '-opengl', '-r0');
+
+else
+    print(file_name,'-dpdf', '-r0')
+end
+
 end
 
