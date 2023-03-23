6ead84c8d578386f75f8c3b4d2be3f1bbbc6368d
mahdiofhijaz <mahdi.hijazi@epfl.ch>
2023-03-23 15:04:32 +0100

    print2pdf now supports Matlab versions older than 2021b

--- Useful_tools/print2pdf.m
+++ Useful_tools/print2pdf.m
@@ -23,12 +23,25 @@ set(gcf,...
 
 % set(gcf,'PaperPositionMode','Auto','PaperUnits','Inches','PaperSize',[screenposition(3), screenposition(4)])
 
-if isOpengl == 1
-    print(file_name, '-dpdf', '-opengl', '-r0');
+matVersion = version('-release'); % Get Matlab version
+versionNbr = str2double(matVersion(1:4));
+versionLtr = matVersion(5);
 
+% Options for function were changed in 2021b
+if versionNbr > 2021 || (versionNbr == 2021 && versionLtr=='b') 
+    if isOpengl == 1
+        print(file_name, '-dpdf', '-image', '-r0');
+    
+    else
+        print(file_name,'-dpdf', '-vector')
+    end
 else
-    print(file_name,'-dpdf', '-vector')
+    if isOpengl == 1
+        print(file_name, '-dpdf', '-opengl', '-r0');
+    
+    else
+        print(file_name,'-dpdf', '-painters')
+    end
 end
-
 end
 
